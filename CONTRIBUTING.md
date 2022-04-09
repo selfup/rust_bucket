6db68c8f7c4198bc0e868af07bbbcf3be94e4ae0
selfup <boudinot.regis@yahoo.com>
2022-04-09 15:17:18 -0500

    update to stable focused API - update deps - use criterion for bench
    get off of travis - move to github actions next
    
    add github action
    
    modify github action
    
    update to point to correct root branch
    
    remove black_box from use statement in criterion bench
    
    update CONTRIBUTING and some formating in lib.rs
    
    reduce benchmark time to reduce SSD destruction
    
    update yml
    
    update yml
    
    actions will trigger?

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,46 +6,5 @@
 * If the issue is a feature request, fork the repo, and make a PR that does not break core functionality and ensures that all tests still pass
 * If you write a new feature (database API), testing is required.
 * If your language is offensive, forget about it!
-* If what you submit in a PR does not work on Windows, OSX, or Linux, then you must make sure that it does!
-
-### If your are on Windows/OSX/Linux: 
-
-*Travis CI will check your commits on a PR.*
-
-But to avoid failures on stable (**if you are unsure**) here is a Vagrantfile that you should setup!
-
-**Please change the `<PATH_TO_YOUR_LOCAL_RUST_BUCKET_FORK_OR_PROJECT>` to your hosts project path!**
-
-```
-$script = <<SCRIPT
-  sudo apt-get update
-  sudo /usr/sbin/update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-  sudo curl -s https://static.rust-lang.org/rustup.sh | sh -s -- --channel=stable
-  cd /home/vagrant/rust_bucket && cargo test
-SCRIPT
-
-Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, :inline => $script
-  config.vm.synced_folder "<PATH_TO_YOUR_LOCAL_RUST_BUCKET_FORK_OR_PROJECT>", "/home/vagrant/rust_bucket"
-  config.vm.provider "virtualbox" do |vb|
-    vb.memory = "1024"
-  end
-end
-```
-
-### Steps to follow when using Vagrant:
-
-`vagrant up`
-
-Now before pushing up a commit do the following:
-
-`vagrant ssh`
-
-`cd rust_bucket && cargo test`
-
-If all the tests pass, go ahead and push to your branch origin.
-
-Travis CI will let us know everything is good to go!
 
 ### This is a WIP and more requirements may be added in the future!
--- src/lib.rs
+++ src/lib.rs
@@ -7,106 +7,112 @@
 // file may not be copied, modified, or distributed except according to those
 // terms.
 
-#![cfg_attr(all(test, feature = "benchmarks"), feature(test))]
-
-extern crate serde_json;
 extern crate serde;
+extern crate serde_json;
 
-use std::io;
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
-use std::path::Path;
-use serde::Deserialize;
+use std::io;
 use std::io::prelude::*;
-use serde::ser::Serialize;
-use std::collections::HashMap;
-
-// sc is the user defined schema
-mod sc;
+use std::path::Path;
 
 pub mod errors;
-use errors::{Result, Error};
+use errors::{Error, Result};
 
-// Build-script-generated instances for the Data type
-include!(concat!(env!("OUT_DIR"), "/data.rs"));
+// Structure for data storage *********************************************************************
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct TableData<T: Serialize> {
+    pub table: String,
+    pub next_id: String,
+    pub records: HashMap<String, T>,
+}
 
 // Public functions *******************************************************************************
 
 pub fn update_table<T: Serialize>(table: &str, t: &T) -> Result<()> {
-    let serialized = try!(serde_json::to_string(&create_base_data(table, t)));
+    let serialized = serde_json::to_string(&create_base_data(table, t))?;
     let db_table = Path::new("./db").join(table);
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(serialized.as_bytes()));
+
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(serialized.as_bytes())?;
 
     Ok(())
 }
 
 pub fn create_table<T: Serialize>(table: &str, t: &T) -> Result<()> {
-    try!(create_db_dir());
+    create_db_dir()?;
 
-    let serialized = try!(serde_json::to_string(&create_base_data(table, t)));
+    let serialized = serde_json::to_string(&create_base_data(table, t))?;
     let db_table = Path::new("./db").join(table);
 
     if db_table.exists() {
         return Ok(());
     }
 
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(serialized.as_bytes()));
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(serialized.as_bytes())?;
 
     Ok(())
 }
 
 pub fn create_empty_table<T: Serialize>(table: &str) -> Result<()> {
-    try!(create_db_dir());
+    create_db_dir()?;
 
     let record: HashMap<String, T> = HashMap::new();
 
-    let data = Data {
+    let data = TableData {
         table: table.to_string(),
         next_id: "0".to_string(),
         records: record,
     };
 
-    let serialized = try!(serde_json::to_string(&data));
+    let serialized = serde_json::to_string(&data)?;
     let db_table = Path::new("./db").join(table);
 
     if db_table.exists() {
         return Ok(());
     }
 
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(serialized.as_bytes()));
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(serialized.as_bytes())?;
 
     Ok(())
 }
 
 pub fn read_table(table: &str) -> Result<String> {
-    let db_table = Path::new("./db").join(table);
+    let db_table = Path::new("./db").join(table.to_owned());
+
     let mut file = match File::open(db_table) {
-        Ok(f) => f,
-        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+        Ok(file) => file,
+        Err(ref err) if err.kind() == io::ErrorKind::NotFound => {
             return Err(Error::NoSuchTable(table.to_owned()))
         }
-        Err(e) => return Err(Error::Io(e)),
+        Err(err) => return Err(Error::Io(err)),
     };
 
     let mut buffer = String::new();
-    try!(file.read_to_string(&mut buffer));
+    file.read_to_string(&mut buffer)?;
 
     Ok(buffer)
 }
 
 pub fn drop_table(table: &str) -> io::Result<()> {
-    let t = Path::new("./db").join(table);
-    try!(fs::remove_file(t));
+    let table_path = Path::new("./db").join(table);
+    fs::remove_file(table_path)?;
 
     Ok(())
 }
 
-pub fn append_records<T: Serialize + Deserialize>(table: &str, t: T) -> Result<()> {
-    let mut data = try!(get_table(table));
-    let increased_next_id = try!(data.next_id.parse::<i32>());
+pub fn append_records<T>(table: &str, t: T) -> Result<()>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let mut data = get_table(table)?;
+    let increased_next_id = data.next_id.parse::<i32>()?;
     let new_id = increased_next_id + 1;
 
     data.records.insert(increased_next_id.to_string(), t);
@@ -115,36 +121,56 @@ pub fn append_records<T: Serialize + Deserialize>(table: &str, t: T) -> Result<(
     upgrade_table(table, &data)
 }
 
-pub fn get_table<T: Serialize + Deserialize>(table: &str) -> Result<Data<T>> {
-    serde_json::from_str(&try!(read_table(table))).map_err(Error::from)
+pub fn get_table<T>(table: &str) -> Result<TableData<T>>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let result = read_table(table)?;
+
+    serde_json::from_str(&result).map_err(Error::from)
 }
 
-pub fn get_table_records<T: Serialize + Deserialize>(table: &str) -> Result<HashMap<String, T>> {
-    Ok(try!(get_table(table)).records)
+pub fn get_table_records<T>(table: &str) -> Result<HashMap<String, T>>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    Ok(get_table(table)?.records)
 }
 
-pub fn find<T: Serialize + Deserialize>(table: &str, id: &str) -> Result<T> {
-    try!(get_table_records(table)).remove(id).ok_or(Error::NoSuchKey)
+pub fn find<T>(table: &str, id: &str) -> Result<T>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    get_table_records(table)?.remove(id).ok_or(Error::NoSuchKey)
 }
 
-pub fn delete<T: Serialize + Deserialize>(table: &str, id: &str) -> Result<()> {
-    let mut current_table: HashMap<String, T> = try!(get_table_records(table));
+pub fn delete<T>(table: &str, id: &str) -> Result<()>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let mut current_table: HashMap<String, T> = get_table_records(table)?;
     current_table.remove(id);
     update_table(table, &current_table)
 }
 
-pub fn json_find<T: Serialize + Deserialize>(table: &str, id: &str) -> Result<String> {
-    let incoming_record: T = try!(find(table, id));
+pub fn json_find<T>(table: &str, id: &str) -> Result<String>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let incoming_record: T = find(table, id)?;
     serde_json::to_string(&incoming_record).map_err(Error::from)
 }
 
-pub fn json_table_records<T: Serialize + Deserialize>(table: &str) -> Result<String> {
-    let records: HashMap<String, T> = try!(get_table_records(table));
+pub fn json_table_records<T>(table: &str) -> Result<String>
+where
+    T: for<'a> Deserialize<'a> + Serialize,
+{
+    let records: HashMap<String, T> = get_table_records(table)?;
     serde_json::to_string(&records).map_err(Error::from)
 }
 
 pub fn store_json(table: &str, json: &str) -> Result<()> {
-    try!(create_db_dir());
+    create_db_dir()?;
 
     let db_table = Path::new("./db").join(table);
 
@@ -152,19 +178,19 @@ pub fn store_json(table: &str, json: &str) -> Result<()> {
         return Ok(());
     }
 
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(json.as_bytes()));
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(json.as_bytes())?;
 
     Ok(())
 }
 
 pub fn update_json(table: &str, json: &str) -> Result<()> {
-    try!(create_db_dir());
+    create_db_dir()?;
 
     let db_table = Path::new("./db").join(table);
 
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(json.as_bytes()));
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(json.as_bytes())?;
 
     Ok(())
 }
@@ -172,20 +198,20 @@ pub fn update_json(table: &str, json: &str) -> Result<()> {
 // Private functions ******************************************************************************
 
 fn upgrade_table<T: Serialize>(table: &str, t: &T) -> Result<()> {
-    let serialized = try!(serde_json::to_string(t));
+    let serialized = serde_json::to_string(t)?;
     let db_table = Path::new("./db").join(table);
 
-    let mut buffer = try!(File::create(db_table));
-    try!(buffer.write_all(serialized.as_bytes()));
+    let mut buffer = File::create(db_table)?;
+    buffer.write_all(serialized.as_bytes())?;
 
     Ok(())
 }
 
-fn create_base_data<T: Serialize>(table: &str, t: T) -> Data<T> {
+fn create_base_data<T: Serialize>(table: &str, t: T) -> TableData<T> {
     let mut record = HashMap::new();
     record.insert("0".to_string(), t);
 
-    Data {
+    TableData {
         table: table.to_string(),
         next_id: "1".to_string(),
         records: record,
@@ -205,14 +231,19 @@ fn create_db_dir() -> io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use sc;
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    pub struct Coordinates {
+        pub x: i32,
+        pub y: i32,
+    }
 
     #[test]
     fn it_can_create_update_and_drop_a_table_and_take_any_struct_to_add_data() {
-        let a = sc::Coordinates { x: 42, y: 9000 };
-        let b = sc::Coordinates { x: 32, y: 8765 };
-        let c = sc::Coordinates { x: 23, y: 900 };
-        let d = sc::Coordinates { x: 105, y: 7382 };
+        let a = Coordinates { x: 42, y: 9000 };
+        let b = Coordinates { x: 32, y: 8765 };
+        let c = Coordinates { x: 23, y: 900 };
+        let d = Coordinates { x: 105, y: 7382 };
 
         let e = "{\"table\":\"test\",\"next_id\":\"1\",\"records\":{\"0\":{\"x\":42,\"y\":9000}}}";
         let f = "{\"table\":\"test\",\"next_id\":\"1\",\"records\":{\"0\":{\"x\":32,\"y\":8765}}}";
@@ -241,7 +272,7 @@ mod tests {
     fn it_can_create_100_tables_and_drop_them_all() {
         for n in 1..101 {
             let table = format!("{}", n);
-            let a = sc::Coordinates { x: 42, y: 9000 };
+            let a = Coordinates { x: 42, y: 9000 };
 
             create_table(&*table, &a).unwrap();
         }
@@ -257,7 +288,7 @@ mod tests {
     fn it_can_create_and_drop_an_empty_table() {
         let table_name = format!("empty");
 
-        create_empty_table::<sc::Coordinates>(&*table_name).unwrap();
+        create_empty_table::<Coordinates>(&*table_name).unwrap();
 
         let contents: String = read_table(&*table_name).unwrap();
         let expected = "{\"table\":\"empty\",\"next_id\":\"0\",\"records\":{}}";
@@ -269,7 +300,7 @@ mod tests {
 
     #[test]
     fn it_can_get_and_find() {
-        let a = sc::Coordinates { x: 42, y: 9000 };
+        let a = Coordinates { x: 42, y: 9000 };
 
         create_table("test3", &a).unwrap();
 
@@ -280,13 +311,13 @@ mod tests {
 
     #[test]
     fn it_can_return_json() {
-        let a = sc::Coordinates { x: 42, y: 9000 };
+        let a = Coordinates { x: 42, y: 9000 };
         create_table("test5", &a).unwrap();
         assert_eq!(a, find("test5", "0").unwrap());
 
         let b: String = read_table("test5").unwrap();
-        let c: String = json_table_records::<sc::Coordinates>("test5").unwrap();
-        let d: String = json_find::<sc::Coordinates>("test5", "0").unwrap();
+        let c: String = json_table_records::<Coordinates>("test5").unwrap();
+        let d: String = json_find::<Coordinates>("test5", "0").unwrap();
 
         let j = "{\"table\":\"test5\",\"next_id\":\"1\",\"records\":{\"0\":{\"x\":42,\"y\":9000}}}";
         assert_eq!(j, b);
@@ -302,77 +333,21 @@ mod tests {
 
     #[test]
     fn it_can_delete_table_data_by_id() {
-        let a = sc::Coordinates { x: 42, y: 9000 };
+        let a = Coordinates { x: 42, y: 9000 };
 
         create_table("test6", &a).unwrap();
 
         assert_eq!(a, find("test6", "0").unwrap());
 
-        let del = delete::<sc::Coordinates>;
+        let del = delete::<Coordinates>;
         del("test6", "0").unwrap();
 
         let table = read_table("test6").unwrap();
-        assert_eq!(table,
-                   "{\"table\":\"test6\",\"next_id\":\"1\",\"records\":{\"0\":{}}}");
+        assert_eq!(
+            table,
+            "{\"table\":\"test6\",\"next_id\":\"1\",\"records\":{\"0\":{}}}"
+        );
 
         drop_table("test6").unwrap();
-    }
-}
-
-#[cfg(all(feature = "benchmarks", test))]
-mod benchmarks {
-    extern crate test;
-
-    use self::test::Bencher;
-
-    use super::*;
-    use sc;
-
-    #[bench]
-    fn bench_create_table(b: &mut Bencher) {
-        let object = sc::Coordinates { x: 42, y: 9000 };
-
-        b.iter(|| create_table("test4", &object).unwrap());
-    }
-
-    #[bench]
-    fn bench_update_table(b: &mut Bencher) {
-        let object = sc::Coordinates { x: 42, y: 9000 };
-
-        b.iter(|| update_table("test2", &object).unwrap());
-    }
-
-    #[bench]
-    fn bench_read_table(b: &mut Bencher) {
-        b.iter(|| read_table("test2").unwrap());
-    }
-
-    #[bench]
-    fn bench_json_table_records(b: &mut Bencher) {
-        let a = json_table_records::<sc::Coordinates>;
-
-        b.iter(|| a("test2").unwrap());
-    }
-
-    #[bench]
-    fn bench_json_find(b: &mut Bencher) {
-        let a = json_find::<sc::Coordinates>;
-
-        b.iter(|| a("test2", "0").unwrap());
-    }
-
-    #[bench]
-    fn bench_find(b: &mut Bencher) {
-        let a = find::<sc::Coordinates>;
-        b.iter(|| a("test2", "0").unwrap());
-    }
-
-    #[bench]
-    fn bench_store_update_read_and_delete_json(b: &mut Bencher) {
-        b.iter(|| store_json("test7", "{\"x\":42,\"y\":9000}}}").unwrap());
-
-        update_json("test7", "{\"x\":45,\"y\":9876}}}").unwrap();
-        read_table("test7").unwrap();
-        drop_table("test7").unwrap();
     }
 }
