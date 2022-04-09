a58887633df64ac2c5913cdbf9b5053114fa1583
selfup <boudinot.regis@yahoo.com>
2022-04-09 15:37:01 -0500

    remove black_box from use statement in criterion bench

--- benches/main_benchmark.rs
+++ benches/main_benchmark.rs
@@ -1,6 +1,6 @@
 use rust_bucket::*;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 extern crate serde;
 extern crate serde_json;
